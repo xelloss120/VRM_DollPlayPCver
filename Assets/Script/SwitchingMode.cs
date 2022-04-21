2ae079ec78e375e2e192ef3fd6fa3bc012211939
xelloss120 <xelloss120@hotmail.com>
2022-04-21 23:19:59 +0900

    間接操作モードの切り替え方法見直し

--- Assets/Script/SwitchingMode.cs
+++ Assets/Script/SwitchingMode.cs
@@ -12,7 +12,114 @@ public class SwitchingMode : MonoBehaviour
     {
         if (SelectVRM.Marker == null) return;
 
-        // ラグドール解除
+        ReleaseRagdoll();
+
+        // モードの判定
+        bool FullMode = true;
+        foreach (var marker in SelectVRM.Marker.List)
+        {
+            if (marker.name != "VRIK_Target" && !marker.gameObject.activeInHierarchy)
+            {
+                FullMode = false;
+            }
+        }
+
+        // モードの切り替え
+        if (!FullMode)
+        {
+            // Humanoid > Full
+            foreach (var marker in SelectVRM.Marker.List)
+            {
+                marker.gameObject.SetActive(marker.name != "VRIK_Target" ? true : false);
+                marker.rotation = marker.GetComponent<LinkRotation>().Target.rotation;
+            }
+            SelectVRM.Animator.gameObject.GetComponent<VRIK>().enabled = false;
+            Slider.value = 0.02f;
+        }
+        else
+        {
+            // Full > Humanoid
+            foreach (var marker in SelectVRM.Marker.List)
+            {
+                var primal = marker.GetComponent<LinkRotation>().IsPrimal;
+                marker.gameObject.SetActive(primal ? true : false);
+            }
+            Slider.value = 0.1f;
+        }
+
+        // 揺れ物の有効と無効を切り替え
+        var springs = SelectVRM.RootMarker.GetComponentsInChildren<VRMSpringBone>();
+        foreach (var spring in springs)
+        {
+            spring.enabled = FullMode ? true : false;
+        }
+    }
+
+    public void SetIK()
+    {
+        if (SelectVRM.Marker == null) return;
+
+        ReleaseRagdoll();
+
+        // Humanoidモード化
+        foreach (var marker in SelectVRM.Marker.List)
+        {
+            var primal = marker.GetComponent<LinkRotation>().IsPrimal;
+            marker.gameObject.SetActive(primal ? true : false);
+        }
+        Slider.value = 0.1f;
+
+        // 揺れ物の有効化
+        var springs = SelectVRM.RootMarker.GetComponentsInChildren<VRMSpringBone>();
+        foreach (var spring in springs)
+        {
+            spring.enabled = true;
+        }
+
+        // IKモード化
+        foreach (var marker in SelectVRM.Marker.List)
+        {
+            marker.gameObject.SetActive(marker.name == "VRIK_Target" ? true : false);
+        }
+        var count = SelectVRM.Marker.List.Count;
+        SelectVRM.Marker.List[count - 10].position = SelectVRM.Marker.List[0].position;
+        SelectVRM.Marker.List[count - 10].rotation = SelectVRM.Marker.List[0].GetComponent<LinkRotation>().Target.rotation;
+        SelectVRM.Marker.List[count - 9].position = SelectVRM.Marker.List[2].position;
+        SelectVRM.Marker.List[count - 9].rotation = SelectVRM.Marker.List[2].GetComponent<LinkRotation>().Target.rotation;
+        SelectVRM.Marker.List[count - 8].position = SelectVRM.Marker.List[8].position;
+        SelectVRM.Marker.List[count - 8].rotation = SelectVRM.Marker.List[8].GetComponent<LinkRotation>().Target.rotation;
+        SelectVRM.Marker.List[count - 7].position = SelectVRM.Marker.List[11].position;
+        SelectVRM.Marker.List[count - 7].rotation = SelectVRM.Marker.List[11].GetComponent<LinkRotation>().Target.rotation;
+        var leftToes = SelectVRM.Animator.GetBoneTransform(HumanBodyBones.LeftToes);
+        if (leftToes != null)
+        {
+            // FootだとズレるのでToesがあれば使う
+            SelectVRM.Marker.List[count - 6].position = leftToes.position;
+            SelectVRM.Marker.List[count - 6].rotation = leftToes.rotation;
+        }
+        else
+        {
+            SelectVRM.Marker.List[count - 6].position = SelectVRM.Marker.List[14].position;
+            SelectVRM.Marker.List[count - 6].rotation = SelectVRM.Marker.List[14].GetComponent<LinkRotation>().Target.rotation;
+        }
+        var rightToes = SelectVRM.Animator.GetBoneTransform(HumanBodyBones.RightToes);
+        if (rightToes != null)
+        {
+            // FootだとズレるのでToesがあれば使う
+            SelectVRM.Marker.List[count - 5].position = rightToes.position;
+            SelectVRM.Marker.List[count - 5].rotation = rightToes.rotation;
+        }
+        else
+        {
+            SelectVRM.Marker.List[count - 5].position = SelectVRM.Marker.List[17].position;
+            SelectVRM.Marker.List[count - 5].rotation = SelectVRM.Marker.List[17].GetComponent<LinkRotation>().Target.rotation;
+        }
+        SelectVRM.Animator.gameObject.GetComponent<VRIK>().enabled = true;
+    }
+
+    void ReleaseRagdoll()
+    {
+        // ラグドール化解除
         var joints = SelectVRM.Animator.gameObject.GetComponentsInChildren<ConfigurableJoint>();
         foreach (var joint in joints)
         {
@@ -41,98 +148,6 @@ public class SwitchingMode : MonoBehaviour
         foreach (var mesh in meshs)
         {
             Destroy(mesh);
-        }
-
-        // モードの判定
-        bool HumanoidMode = true;
-        bool FullMode = true;
-        foreach (var marker in SelectVRM.Marker.List)
-        {
-            var primal = marker.GetComponent<LinkRotation>().IsPrimal;
-            if (primal && !marker.gameObject.activeInHierarchy)
-            {
-                HumanoidMode = false;
-            }
-            if (marker.name != "VRIK_Target" && !marker.gameObject.activeInHierarchy)
-            {
-                FullMode = false;
-            }
-        }
-        if (FullMode)
-        {
-            HumanoidMode = false;
-        }
-
-        // モードの切り替え
-        if (HumanoidMode)
-        {
-            // Humanoid > VRIK
-            foreach (var marker in SelectVRM.Marker.List)
-            {
-                marker.gameObject.SetActive(marker.name == "VRIK_Target" ? true : false);
-            }
-            var count = SelectVRM.Marker.List.Count;
-            SelectVRM.Marker.List[count - 10].position = SelectVRM.Marker.List[0].position;
-            SelectVRM.Marker.List[count - 10].rotation = SelectVRM.Marker.List[0].GetComponent<LinkRotation>().Target.rotation;
-            SelectVRM.Marker.List[count - 9].position = SelectVRM.Marker.List[2].position;
-            SelectVRM.Marker.List[count - 9].rotation = SelectVRM.Marker.List[2].GetComponent<LinkRotation>().Target.rotation;
-            SelectVRM.Marker.List[count - 8].position = SelectVRM.Marker.List[8].position;
-            SelectVRM.Marker.List[count - 8].rotation = SelectVRM.Marker.List[8].GetComponent<LinkRotation>().Target.rotation;
-            SelectVRM.Marker.List[count - 7].position = SelectVRM.Marker.List[11].position;
-            SelectVRM.Marker.List[count - 7].rotation = SelectVRM.Marker.List[11].GetComponent<LinkRotation>().Target.rotation;
-            var leftToes = SelectVRM.Animator.GetBoneTransform(HumanBodyBones.LeftToes);
-            if (leftToes != null)
-            {
-                // FootだとズレるのでToesがあれば使う
-                SelectVRM.Marker.List[count - 6].position = leftToes.position;
-                SelectVRM.Marker.List[count - 6].rotation = leftToes.rotation;
-            }
-            else
-            {
-                SelectVRM.Marker.List[count - 6].position = SelectVRM.Marker.List[14].position;
-                SelectVRM.Marker.List[count - 6].rotation = SelectVRM.Marker.List[14].GetComponent<LinkRotation>().Target.rotation;
-            }
-            var rightToes = SelectVRM.Animator.GetBoneTransform(HumanBodyBones.RightToes);
-            if (rightToes != null)
-            {
-                // FootだとズレるのでToesがあれば使う
-                SelectVRM.Marker.List[count - 5].position = rightToes.position;
-                SelectVRM.Marker.List[count - 5].rotation = rightToes.rotation;
-            }
-            else
-            {
-                SelectVRM.Marker.List[count - 5].position = SelectVRM.Marker.List[17].position;
-                SelectVRM.Marker.List[count - 5].rotation = SelectVRM.Marker.List[17].GetComponent<LinkRotation>().Target.rotation;
-            }
-            SelectVRM.Animator.gameObject.GetComponent<VRIK>().enabled = true;
-        }
-        else if (!FullMode)
-        {
-            // VRIK > Full
-            foreach (var marker in SelectVRM.Marker.List)
-            {
-                marker.gameObject.SetActive(marker.name != "VRIK_Target" ? true : false);
-                marker.rotation = marker.GetComponent<LinkRotation>().Target.rotation;
-            }
-            SelectVRM.Animator.gameObject.GetComponent<VRIK>().enabled = false;
-            Slider.value = 0.02f;
-        }
-        else
-        {
-            // Full > Humanoid
-            foreach (var marker in SelectVRM.Marker.List)
-            {
-                var primal = marker.GetComponent<LinkRotation>().IsPrimal;
-                marker.gameObject.SetActive(primal ? true : false);
-            }
-            Slider.value = 0.1f;
-        }
-
-        // 揺れ物の有効と無効を切り替え
-        var springs = SelectVRM.RootMarker.GetComponentsInChildren<VRMSpringBone>();
-        foreach (var spring in springs)
-        {
-            spring.enabled = HumanoidMode || FullMode ? true : false;
         }
     }
 }
